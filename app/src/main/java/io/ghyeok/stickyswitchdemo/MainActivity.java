ff3218e0c437555438772c8f0d5cbd9e7c4357c6
GwonHyeok <me@ghyeok.io>
2017-03-14 15:26:43 +0900

    demo application implement Selected Change Listener

--- app/src/main/java/io/ghyeok/stickyswitchdemo/MainActivity.java
+++ app/src/main/java/io/ghyeok/stickyswitchdemo/MainActivity.java
@@ -2,13 +2,29 @@ package io.ghyeok.stickyswitchdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.jetbrains.annotations.NotNull;
+
+import io.ghyeok.stickyswitch.widget.StickySwitch;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set Selected Change Listener
+        StickySwitch stickySwitch = (StickySwitch) findViewById(R.id.sticky_switch);
+        stickySwitch.setOnSelectedChangeListener(new StickySwitch.OnSelectedChangeListener() {
+            @Override
+            public void onSelectedChange(@NotNull StickySwitch.Direction direction) {
+                Log.d(TAG, "Now Selected : " + direction.name());
+            }
+        });
     }
 
 }
